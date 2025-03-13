b252869ae4de51e473e112a94eda833e1c628fba
garrettmichae1 <garrettmichael07@icloud.com>
2025-03-13 10:17:12 -0500

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,51 @@
+package csci2010.adjacencymatrixdemo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Garrett Woodside
+ */
+public class Graph {
+    
+     ArrayList<Node> nodes;
+     
+     
+    int[][] matrix;
+    
+    Graph(int size){
+        nodes = new ArrayList<>();
+        matrix = new int[size][size];
+    }
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+    public void addEdge(int src, int dst){
+        matrix[src][dst] = 1;
+    }
+    public boolean checkEdge(int src, int dst){
+        if(matrix[src][dst] == 1){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+    public void print(){
+        System.out.print("  ");
+        for(Node node : nodes){
+            System.out.println(node.data + " ");
+        }
+        System.out.println();
+        
+        
+        for(int i = 0; i < matrix.length; i++){
+            System.out.println(nodes.get(i).data + " ");
+            for(int j = 0;  j < matrix[i].length; j++){
+                System.out.print(matrix[i][j]);
+        }
+            System.out.println();
+        }
+    }
+}
+
---
+++ Node.java
@@ -0,0 +1,15 @@
+package csci2010.adjacencymatrixdemo;
+
+/**
+ *
+ * @author Garrett Woodside
+ */
+public class Node {
+    
+    char data;
+    
+    Node(char  data){
+          this.data = data;
+    }
+    
+}
